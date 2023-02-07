1efd38a546c733228ac71d32195f09d6132d8f0f
Alonge Oluwatobiloba <104198149+Oluwatobiloba2905@users.noreply.github.com>
2023-02-07 16:56:24 +0100

    image

---
+++ script.js
@@ -0,0 +1,38 @@
+// const first = () => {
+//     document.getElementById("slide").src = "Img/2V4A5118.jpg";
+// }
+// setInterval(first, 2000);
+
+// const second = () => {
+//     document.getElementById("slide").src = "Img/2V4A5125.jpg";
+// }
+// setInterval(second, 4000);
+
+// const third = () => {
+//     document.getElementById('slide').src = 'Img/2V4A5123.jpg';
+// }
+// setInterval(third, 6000);
+
+// const fourth = () => {
+//     document.getElementById('slide').src = 'Img/2V4A5126.jpg';
+// }
+// setInterval(fourth, 8000);
+
+// const sixth = () => {
+//     document.getElementById('slide').src = 'Img/2V4A5132.jpg';
+// }
+// setInterval(sixth, 10000);
+// const seventh = () => {
+//     document.getElementById('slide').src = 'Img/2V4A5219.jpg';
+// }
+// setInterval(seventh , 10000);
+
+// const eight = () => {
+//     document.getElementById('slide').src = 'Img/2V4A5221.jpg';
+// }
+// setInterval(eight, 10000);
+
+// const ninth = () => {
+//     document.getElementById('slide').src = 'Img/2V4A5257.jpg';
+// }
+// setInterval(ninth, 10000);
